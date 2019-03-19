a8e7dc7b94b62fb36e59d10a72e41e786c7f0dd9
David Padilla <davidpadilla.f22@gmail.com>
2019-03-18 22:10:32 -0500

    Implementation of BurgerIngredient component
    BurgerIngredient component was implemented to visually show the ingredients
    on the screen, and also the BurgerBuilder component to build the burger.
    
    Changes:
    
    - Also a validations was added to the component's props
    
    - The styles were created for every ingredient and added to the component
    
    - The ingredient change depends on the "type" prop passed to the component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,12 @@ import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 
 const layout = props => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+  </Aux>
 );
 
 export default layout;
