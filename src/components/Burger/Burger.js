118487a9fad054cd5d1cc1126db3c051e3d64ed2
David Padilla <davidpadilla.f22@gmail.com>
2019-03-18 23:06:18 -0500

    Add Ingredients Message
    A message asking the user to add ingredients was created whenever
    the burger has no ingredients yet

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => (
         <BurgerIngredient key={igKey + i} type={igKey} />
       ))
-    });
+    }) // [[{},{}], [{}], [{},{}], [{},{},{}]] es un arreglo de arreglos
+    // en donde cada arreglo pertenece a un tipo de ingrediente y
+    // el numero de objetos dentro es la cantidad de ese ingrediente
+    .reduce((arr, el) => {
+      return  arr.concat(el); // [{}, {}, {}, {}, ...] en donde cada objeto
+      // es un elemento JSX perteneciente a los ingredientes
+    }, []);
+
+
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>;
+  }
 
   return (
     <div className={classes.Burger}>
