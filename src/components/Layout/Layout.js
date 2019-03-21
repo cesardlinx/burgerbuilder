b9b0a5305c2d61c613c0e43aadad561ba3341efe
David Padilla <davidpadilla.f22@gmail.com>
2019-03-20 20:30:52 -0500

    Drawer Toggle Button implementation
    The drawer toggle button was implemented so that the user can
    toggle the side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,16 +7,22 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHandler = () => {this.setState({showSideDrawer: false})}
+
+  toggleMenuHandler = () => {
+    this.setState(prevState => ({
+      showSideDrawer: !prevState.showSideDrawer
+    }));
+  }
 
   render() {
     return (
       <Aux>
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
-        <Toolbar />
+        <Toolbar toggleMenu={this.toggleMenuHandler}/>
         <main className={classes.Content}>
             {this.props.children}
         </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 
 const Toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
+      <DrawerToggle clicked={props.toggleMenu}/>
       <Logo height="80%"/>
       <nav className={classes.DesktopOnly}>
         <NavigationItems />
