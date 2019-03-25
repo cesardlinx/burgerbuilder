d3ae4d46dd126bfb011c711eb163373192aea641
David Padilla <davidpadilla.f22@gmail.com>
2019-03-25 18:42:22 -0500

    Checkout Component added
    The Checkout component was added to contain a graphic view of the
    burger selection made by the user
    
    Changes:
    - Proptypes were used on the Button component}
    - React Router was installed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -23,6 +23,7 @@ const OrderSummary = (props) => {
       <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import classes from './Button.module.css';
+import PropTypes from 'prop-types';
 
-const Button = (props) => (
+const Button = ({ btnType, clicked, children}) => (
   <button
-    className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>{props.children}</button>
+    className={[classes.Button, classes[btnType]].join(' ')}
+    onClick={clicked}>{children}</button>
 );
+
+Button.propTypes = {
+  btnType: PropTypes.oneOf(['Success', 'Danger'])
+}
 
 export default Button;
