0b7766c95365ef939669743eb959d0ffc5793d17
David Padilla <davidpadilla.f22@gmail.com>
2019-04-07 16:55:03 -0500

    Guarding the routes
    The routes for:
    - Logout
    - Checkout
    - Orders
    Are now protected in the frontend agains the unauthenticated users

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
@@ -16,21 +16,42 @@ class App extends Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/auth" component={Auth} />
+          <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -38,4 +59,4 @@ const mapDispatchToProps = dispatch => {
   }
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
